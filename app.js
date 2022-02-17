603c966f5642b639ff767c0d9c92c17170c60aae
tool2000 <shareit@hanmail.net>
2022-02-17 10:45:15 +0900

    express engine start..

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello, world')
+})
+// req: request(요청), 사용자의 브라우저 정보, 질문(주소창), 로그인정보 
+// response(응답) : 사이트 내용 html
+
+app.listen(3000, () => {
+    console.log('3000번 포트에서 웹서버를 실행중입니다...')
+})
